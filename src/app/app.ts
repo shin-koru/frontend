ed9ad7ff1085c859627a1ed3f293470c8b9eb2ac
oralkh <oralhan.seilhan07@gmail.com>
2026-09-08 01:24:04 +0500

    feat: primeng was added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [ButtonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
